9dd7f612b9205ebc636d4d1b139c41a2ab2f4b2e
vinay mocharla <vinaymocharla8@gmail.com>
2020-07-13 20:22:45 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,rem,rev=0;
+            
+            num = Convert.ToInt16(Console.ReadLine());
+            int temp = num;
+            while (num!=0)
+            {
+                rem = num % 10;
+                rev = rev * 10 + rem;
+                num = num / 10;
+
+            }
+            if(temp==rev)
+            {
+                Console.WriteLine("the  number is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine("the  number is not  a palindrome");
+
+            }
+            
+
+
+        }
+    }
+}
